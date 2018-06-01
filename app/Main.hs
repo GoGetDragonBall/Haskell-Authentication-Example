8b25f5969896ae45f1757949e33fd507fb71a449
root <root@DESKTOP-C1JN428.localdomain>
2018-06-01 21:15:36 +0900

    Add login api type

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
-import Lib
+import GHC.Generics
+import Data.Aeson    
+import Network.Wai.Handler.Warp    
+import Servant
+
+data LoginParams = LoginParams
+    { id :: String
+    , pw :: String
+    } deriving (Generic)
+
+instance FromJSON LoginParams
+
+data LoginResponseBody = LoginResponseBody
+    { field :: String } deriving (Generic)
+
+instance ToJSON LoginResponseBody
+
+type LoginAPI = "login" :> ReqBody '[JSON] LoginParams :> Post '[JSON] LoginResponseBody
+
+server :: Server LoginAPI
+server = _todo
+
+api :: Proxy LoginAPI
+api = Proxy
+
+app :: Application
+app = serve api server
 
 main :: IO ()
-main = startApp
+main = run 8080 app
+-- main = putStrLn "Hello World"
